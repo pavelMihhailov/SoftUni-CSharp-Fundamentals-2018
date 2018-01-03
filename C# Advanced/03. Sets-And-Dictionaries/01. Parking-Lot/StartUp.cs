1211165aedfbfab26a95a3d82be2ea4b77e3c3b0
pavelNM <papi1998@mail.bg>
2018-01-04 00:06:45 +0200

    Added lab

---
+++ C# Advanced/03. Sets-And-Dictionaries/01. Parking-Lot/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var input = Console.ReadLine().Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+        var parking = new SortedSet<string>();
+
+        while (input[0] != "END")
+        {
+            var carNumber = input[1];
+            var direction = input[0];
+
+            if (direction == "IN")
+            {
+                parking.Add(carNumber);
+            }
+            else
+            {
+                if (parking.Contains(carNumber)) parking.Remove(carNumber);
+            }
+            input = Console.ReadLine().Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+        }
+        if (parking.Count == 0) Console.WriteLine("Parking Lot is Empty");
+        else
+            foreach (var car in parking)
+                Console.WriteLine(car);
+    }
+}
---
+++ C# Advanced/03. Sets-And-Dictionaries/02. SoftUni-Party/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var input = Console.ReadLine();
+
+        var vipGuests = new SortedSet<string>();
+        var regularGuests = new SortedSet<string>();
+        var allGuests = new SortedSet<string>();
+        var isParty = false;
+
+        while (input != "END")
+        {
+            allGuests.Add(input);
+            if (input == "PARTY") isParty = true;
+
+            if (char.IsDigit(input.First()) && isParty)
+            {
+                vipGuests.Add(input);
+                allGuests.Remove(input);
+            }
+            else if (char.IsLetter(input.First()) && isParty)
+            {
+                regularGuests.Add(input);
+                allGuests.Remove(input);
+            }
+            input = Console.ReadLine();
+        }
+        if (allGuests.Count > 0)
+        {
+            Console.WriteLine(allGuests.Count);
+            foreach (var guest in allGuests)
+                Console.WriteLine(guest);
+        }
+        else
+        {
+            Console.WriteLine("0");
+        }
+    }
+}
