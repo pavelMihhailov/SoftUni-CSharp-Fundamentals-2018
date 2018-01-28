0c3698c16a66f4f92f5f7254de4a6e9405d33a31
pavelNM <papi1998@mail.bg>
2018-01-28 20:38:15 +0200

    Made project

---
+++ C# Advanced/BashSoft/BashSoft/Launcher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BashSoft
+{
+    public class Launcher
+    {
+        public static void Main()
+        {
+            Tester.CompareContent(@"C:\Users\Kristofar Hristov\Desktop\BashSoft-FirstWeek\BashSoft-FirstWeek\actual.txt", @"C:\Users\Kristofar Hristov\Desktop\BashSoft-FirstWeek\BashSoft-FirstWeek\expected.txt");
+        }
+    }
+}
