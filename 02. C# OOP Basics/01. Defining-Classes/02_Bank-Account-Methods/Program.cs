de97d0d5abc04c5cc96c07e2a179f33cb81385cf
Pavel <pavel.nm@abv.bg>
2018-02-13 11:46:30 +0200

    Add lab

---
+++ 02. C# OOP Basics/01. Defining-Classes/02_Bank-Account-Methods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        BankAccount acc = new BankAccount();
+
+        acc.ID = 1;
+        acc.Deposit(15);
+        acc.Withdraw(10);
+
+        Console.WriteLine(acc);
+    }
+}
