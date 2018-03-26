31537a1986bab4ae7b283cf56d7eae080d500915
pavelNM <papi1998@mail.bg>
2018-03-26 20:17:14 +0300

    Added lab

---
+++ C# OOP Advanced/05_Iterators-And-Comparators/01_Library/Models/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Book : IComparable<Book>
+{
+    public Book(string title, int year, params string[] authors)
+    {
+        Title = title;
+        Year = year;
+        Authors = authors;
+    }
+
+    public string Title { get; private set; }
+
+    public int Year { get; private set; }
+
+    public IReadOnlyList<string> Authors { get; set; }
+
+    public int CompareTo(Book other)
+    {
+        var result = this.Year.CompareTo(other.Year);
+        if (result.Equals(0))
+        {
+            result = this.Title.CompareTo(other.Title);
+        }
+        return result;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Title} - {this.Year}";
+    }
+}
---
+++ C# OOP Advanced/05_Iterators-And-Comparators/01_Library/Models/Library.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Library : IEnumerable<Book>
+{
+    public Library(params Book[] books)
+    {
+        Books = new List<Book>(books);
+    }
+
+    public List<Book> Books { get; set; }
+
+    public IEnumerator<Book> GetEnumerator()
+    {
+        return new LibraryIterator(Books);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    private class LibraryIterator : IEnumerator<Book>
+    {
+        public LibraryIterator(IEnumerable<Book> books)
+        {
+            Reset();
+            _books = new List<Book>(books);
+        }
+
+        private readonly List<Book> _books;
+        private int currIndex;
+
+        public bool MoveNext()
+        {
+            currIndex++;
+            return currIndex < _books.Count;
+        }
+
+        public void Reset()
+        {
+            currIndex = -1;
+        }
+
+        public Book Current => this._books[currIndex];
+
+        object IEnumerator.Current => this.Current;
+
+        public void Dispose(){}
+    }
+}
